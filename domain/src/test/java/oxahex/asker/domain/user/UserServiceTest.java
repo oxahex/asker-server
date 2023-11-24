10aeee439e5dd7e284975e8ef2359c679b78ba18
oxahex <oxahex@gmail.com>
2023-11-24 19:02:40 +0900

    feat: #7 회원가입 API 기능 구현
    commit 006cbfec5ff4aa47ff6a91a73ce1c12086765154
    Author: oxahex <oxahex@gmail.com>
    Date:   Fri Nov 24 19:01:45 2023 +0900
    
        test: 회원가입 기능 테스트 코드 작성
    
        - 중복 Email 검증
    
    commit 6bc5783e61e418fb71f7fe65b3787b373b824fb2
    Author: oxahex <oxahex@gmail.com>
    Date:   Fri Nov 24 19:00:39 2023 +0900
    
        chore: JWT 검증 필터 구조 작성
    
    commit 673961440044f432b807e98d86c2e6d2341217c0
    Author: oxahex <oxahex@gmail.com>
    Date:   Fri Nov 24 18:59:30 2023 +0900
    
        feat: 회원가입 기능 구현
    
    commit 1ca1a0e60566bb0773b6b7cc8797ddaff74aebc7
    Author: oxahex <oxahex@gmail.com>
    Date:   Fri Nov 24 18:59:14 2023 +0900
    
        feat: 유저 도메인 서비스 코드 작성
    
        - 유저 생성
        - 유저 조회
        - email 중복 검증
    
    commit 87ef5b658be4a0aabbb192361c15a8f4d4fa7ec6
    Author: oxahex <oxahex@gmail.com>
    Date:   Fri Nov 24 18:56:27 2023 +0900
    
        chore: login URL 변경
    
    commit 66a80ab62ae0b9e25a53728cd2bc4c4dc121cfb7
    Author: oxahex <oxahex@gmail.com>
    Date:   Fri Nov 24 18:50:50 2023 +0900
    
        feat: 유효성 검사 관련 에러 전역 에러 핸들러 등록
    
    commit 8b08f53f724f49f39a85ec381737cafe263c1542
    Author: oxahex <oxahex@gmail.com>
    Date:   Fri Nov 24 18:49:57 2023 +0900
    
        feat: 유저 관련 에러 도메인 모듈 하위로 이동

---
+++ domain/src/test/java/oxahex/asker/domain/user/UserServiceTest.java
@@ -0,0 +1,76 @@
+package oxahex.asker.domain.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import oxahex.asker.domain.error.exception.UserException;
+import oxahex.asker.domain.error.type.UserError;
+import oxahex.asker.domain.mock.MockObject;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest extends MockObject {
+
+  @InjectMocks
+  private UserService userService;
+
+  @Mock
+  private UserRepository userRepository;
+
+  @Test
+  @DisplayName("유저 생성 - 성공")
+  public void createUser_success() throws Exception {
+
+    // given
+    String name = "test";
+    String email = "test@gmail.com";
+    String password = "1234567890";
+
+    // stub 1: 해당 Email로 가입한 유저 없음
+    given(userRepository.existsByEmail(anyString())).willReturn(false);
+
+    // stub 2: 해당 유저 데이터 DB 저장 성공
+    User user = mockUser(1L, "test");
+    given(userRepository.save(any(User.class))).willReturn(user);
+
+    // when
+    User createdUser = userService.createUser(name, email, password);
+
+    // then: name, email, password, role
+    Assertions.assertEquals(name, createdUser.getName());
+    Assertions.assertEquals(email, createdUser.getEmail());
+    Assertions.assertEquals(password, createdUser.getPassword());
+    Assertions.assertEquals(RoleType.USER, createdUser.getRole());
+  }
+
+  @Test
+  @DisplayName("유저 생성 - 실패: 이미 존재하는 Email로 유저 생성 불가")
+  public void createUser_failure() throws Exception {
+
+    // given
+    String name = "test";
+    String email = "test@gmail.com";
+    String password = "1234567890";
+
+    // stub 1: 해당 Email로 가입한 유저 있음
+    given(userRepository.existsByEmail(anyString())).willReturn(true);
+
+    // when
+    UserException exception = assertThrows(UserException.class,
+        () -> userService.createUser(name, email, password));
+
+    // then
+    Assertions.assertEquals(UserError.ALREADY_EXIST_EMAIL.getStatusCode(),
+        exception.getStatusCode());
+    Assertions.assertEquals(UserError.ALREADY_EXIST_EMAIL.getErrorMessage(),
+        exception.getErrorMessage());
+  }
+}
