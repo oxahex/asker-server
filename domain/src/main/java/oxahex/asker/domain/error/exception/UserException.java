8b08f53f724f49f39a85ec381737cafe263c1542
oxahex <oxahex@gmail.com>
2023-11-24 18:49:57 +0900

    feat: 유저 관련 에러 도메인 모듈 하위로 이동

--- domain/src/main/java/oxahex/asker/domain/error/exception/UserException.java
+++ domain/src/main/java/oxahex/asker/domain/error/exception/UserException.java
@@ -1,17 +1,16 @@
-package oxahex.asker.error.exception;
+package oxahex.asker.domain.error.exception;
 
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
-import oxahex.asker.error.type.UserError;
+import oxahex.asker.domain.error.type.UserError;
 
 @Getter
 public class UserException extends RuntimeException {
 
-  HttpStatus httpStatus;
+  int statusCode;
   String errorMessage;
 
   public UserException(UserError userError) {
-    this.httpStatus = userError.getHttpStatus();
+    this.statusCode = userError.getStatusCode();
     this.errorMessage = userError.getErrorMessage();
   }
 }
