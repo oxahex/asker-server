c65b1a241cf1732ab0c514d0c7d9796dfb2b2309
oxahex <oxahex@gmail.com>
2023-11-30 20:32:57 +0900

    chore: MethodSecurity 어노테이션 제거

--- application/src/main/java/oxahex/asker/config/SecurityConfig.java
+++ application/src/main/java/oxahex/asker/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
