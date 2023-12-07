7cf063425acbd50bc1839820bdbc260630c26413
oxahex <oxahex@gmail.com>
2023-12-07 16:51:18 +0900

    test: 질문하기 기능 테스트 코드

--- application/src/test/java/oxahex/asker/dispatch/AskControllerTest.java
+++ application/src/test/java/oxahex/asker/dispatch/AskControllerTest.java
@@ -7,6 +7,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +16,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+import oxahex.asker.auth.AuthService;
 import oxahex.asker.dispatch.dto.AskDto.AskReqDto;
 import oxahex.asker.domain.ask.Ask;
 import oxahex.asker.domain.ask.AskType;
@@ -43,23 +47,35 @@ class AskControllerTest extends MockUser {
   @MockBean
   private DispatchService dispatchService;
 
+  private User askUser;
+  private User answerUser;
+
+  @BeforeEach
+  public void setup() {
+    this.askUser = userRepository.save(newUser("asker", "1234567890"));
+    this.answerUser = userRepository.save(newUser("answerer", "1234567890"));
+  }
+
   @Test
   @DisplayName("질문 요청 성공 - 로그인하지 않은 유저라도 POST 요청인 경우 특정 유저에게 질문할 수 있다.")
   public void ask_without_login_success() throws Exception {
 
     // given
-    User askUser = userRepository.save(newUser("asker", "1234567890"));
-    User answerUser = userRepository.save(newUser("answerer", "1234567890"));
-
     AskReqDto askReqDto = new AskReqDto();
     askReqDto.setAnswerUserId(answerUser.getId());
     askReqDto.setContents("2L 유저에게 익명 유저가 보내는 질문?");
 
-    Ask ask = Ask.builder().askUser(askUser).contents("contetntt").askType(AskType.USER).build();
+    Ask ask = Ask.builder()
+        .askUser(this.askUser)
+        .contents("asker가 answerer에게 보내는 질문")
+        .askType(AskType.USER)
+        .build();
+
     Dispatch dispatch = Dispatch.builder()
         .ask(ask)
-        .answerUser(answerUser)
+        .answerUser(this.answerUser)
         .build();
+
     given(dispatchService.dispatch(any(), any())).willReturn(dispatch);
 
     String requestBody = objectMapper.writeValueAsString(askReqDto);
@@ -75,13 +91,39 @@ class AskControllerTest extends MockUser {
   }
 
   @Test
+  // setupBefore=TEST_METHOD 메서드 실행 전에 수행, beforeEach 이전
+  // setupBefore=TEST_EXECUTION beforeEach 이후 실 테스트 메서드 이전에
+  @WithUserDetails(value = "asker@gmail.com", setupBefore = TestExecutionEvent.TEST_EXECUTION)
   @DisplayName("질문 요청 성공 - 로그인 유저는 특정 유저에게 질문할 수 있다.")
   public void ask_with_login_user_success() throws Exception {
 
     // given
+    AskReqDto askReqDto = new AskReqDto();
+    askReqDto.setAnswerUserId(answerUser.getId());
+    askReqDto.setContents("asker가 answerer에게 보내는 질문");
+
+    Ask ask = Ask.builder()
+        .askUser(this.askUser)
+        .contents(askReqDto.getContents())
+        .askType(AskType.USER)
+        .build();
+    Dispatch dispatch = Dispatch.builder()
+        .ask(ask)
+        .answerUser(answerUser)
+        .build();
+    given(dispatchService.dispatch(any(), any())).willReturn(dispatch);
+
+    String requestBody = objectMapper.writeValueAsString(askReqDto);
 
     // when
+    ResultActions resultActions = mockMvc.perform(post("/api/asks")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestBody));
+
+    System.out.println(resultActions.andReturn().getResponse().getContentAsString());
+
     // then
+    resultActions.andExpect(status().isCreated());
   }
 
   @Test
