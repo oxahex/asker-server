23df8b90c25778652f00ca17b084c56d8fcef4c6
oxahex <oxahex@gmail.com>
2023-12-08 18:06:38 +0900

    test: 답변 기능 테스트

--- application/src/test/java/oxahex/asker/dispatch/DispatchServiceTest.java
+++ application/src/test/java/oxahex/asker/dispatch/DispatchServiceTest.java
@@ -13,7 +13,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import oxahex.asker.auth.AuthUser;
+import oxahex.asker.dispatch.dto.AnswerDto.AnswerReqDto;
 import oxahex.asker.dispatch.dto.AskDto.AskReqDto;
+import oxahex.asker.domain.answer.Answer;
+import oxahex.asker.domain.answer.AnswerDomainService;
 import oxahex.asker.domain.ask.Ask;
 import oxahex.asker.domain.ask.AskDomainService;
 import oxahex.asker.domain.ask.AskType;
@@ -21,6 +24,8 @@ import oxahex.asker.domain.dispatch.Dispatch;
 import oxahex.asker.domain.dispatch.DispatchDomainService;
 import oxahex.asker.domain.user.User;
 import oxahex.asker.domain.user.UserDomainService;
+import oxahex.asker.error.exception.ServiceException;
+import oxahex.asker.error.type.ServiceError;
 import oxahex.asker.mock.MockUser;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,6 +39,9 @@ class DispatchServiceTest extends MockUser {
 
   @Mock
   private AskDomainService askDomainService;
+
+  @Mock
+  AnswerDomainService answerDomainService;
 
   @Mock
   private DispatchDomainService dispatchDomainService;
@@ -88,7 +96,7 @@ class DispatchServiceTest extends MockUser {
     askReqDto.setAnswerUserId(2L);
     askReqDto.setContents("2L 유저에게 1L 유저가 보내는 질문?");
 
-    // stub 1:
+    // stub 1: 질문 생성 성공
     Ask ask = Ask.builder()
         .id(1L)
         .askUser(authUser.getUser())
@@ -98,7 +106,7 @@ class DispatchServiceTest extends MockUser {
     given(askDomainService.createAsk(any(), anyString()))
         .willReturn(ask);
 
-    // stub 2:
+    // stub 2: 질문 내역 생성 성공
     Dispatch dispatch = Dispatch.builder()
         .ask(ask)
         .answerUser(answerUser)
@@ -112,5 +120,84 @@ class DispatchServiceTest extends MockUser {
     // then
     Assertions.assertEquals(result.getId(), 1L);
     Assertions.assertEquals(result.getAskUser().getId(), 1L);
+  }
+
+  @Test
+  @DisplayName("답변 생성 - 성공")
+  public void dispatch_answer_success() throws Exception {
+
+    // given
+    // 답변할 유저 정보
+    User answerUser = mockUser(1L, "로그인 유저", "1234567890");
+    // 요청 데이터
+    AnswerReqDto answerReqDto = new AnswerReqDto();
+    answerReqDto.setDispatchId(2L);
+    answerReqDto.setContents("3L 질문에 대한 답변");
+    // 질문
+    Ask ask = Ask.builder()
+        .id(3L).contents("질문 내용").build();
+    // 질문 전송 내역
+    Dispatch dispatch = Dispatch.builder()
+        .answerUser(answerUser)
+        .ask(ask)
+        .build();
+
+    // Dispatch 정보 가져옴
+    given(dispatchDomainService.findDispatch(anyLong()))
+        .willReturn(dispatch);
+
+    // 질문에 대한 답변 생성
+    Answer answer = Answer.builder()
+        .answerUser(answerUser)
+        .ask(ask)
+        .contents(answerReqDto.getContents())
+        .build();
+    given(answerDomainService.createAnswer(any(Dispatch.class), anyString()))
+        .willReturn(answer);
+
+    // when
+    Answer createdAnswer = dispatchService.dispatchAnswer(answerUser, answerReqDto);
+
+    // then
+    // 생성된 답변의 작성자 ID가 answerUser ID와 같음
+    Assertions.assertEquals(createdAnswer.getAnswerUser().getId(), answerUser.getId());
+    // 생성된 답변의 내용이 요청한 답변 내용과 같음
+    Assertions.assertEquals(createdAnswer.getContents(), answerReqDto.getContents());
+  }
+
+  @Test
+  @DisplayName("답변 생성 실패 - 본인에게 온 질문이 아닌 경우 답변할 수 없습니다.")
+  public void dispatch_answer_failure_answer_user() throws Exception {
+
+    // given
+    // 답변할 유저 정보
+    User answerUser = mockUser(1L, "로그인 유저", "1234567890");
+    User otherUser = mockUser(2L, "다른 유저", "1234567890");
+    // 요청 데이터
+    AnswerReqDto answerReqDto = new AnswerReqDto();
+    answerReqDto.setDispatchId(2L);
+    answerReqDto.setContents("3L 질문에 대한 답변");
+    // 질문
+    Ask ask = Ask.builder()
+        .id(3L).contents("질문 내용").build();
+    // 질문 전송 내역: 요청 유저와 다른 유저에게 보낸 질문 전송 내역
+    Dispatch dispatch = Dispatch.builder()
+        .answerUser(otherUser)
+        .ask(ask)
+        .build();
+
+    // Dispatch 정보 가져옴
+    given(dispatchDomainService.findDispatch(anyLong()))
+        .willReturn(dispatch);
+
+    // when
+    ServiceException exception = Assertions.assertThrows(ServiceException.class,
+        () -> dispatchService.dispatchAnswer(answerUser, answerReqDto));
+
+    // then
+    Assertions.assertEquals(exception.getHttpStatus(),
+        ServiceError.NO_AUTHORITY_TO_ANSWER.getHttpStatus());
+    Assertions.assertEquals(exception.getErrorMessage(),
+        ServiceError.NO_AUTHORITY_TO_ANSWER.getErrorMessage());
   }
 }
