c535b0584ef79ce6f7bcd721a9a2cd89d7896c12
oxahex <oxahex@gmail.com>
2023-12-13 19:57:22 +0900

    feat: 정렬 타입 지정

---
+++ domain/src/main/java/oxahex/asker/domain/condition/SortType.java
@@ -0,0 +1,35 @@
+package oxahex.asker.domain.condition;
+
+import java.util.Arrays;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import oxahex.asker.domain.error.exception.ConditionException;
+import oxahex.asker.domain.error.type.ConditionError;
+
+@Getter
+@RequiredArgsConstructor
+public enum SortType {
+  
+  ASC("asc"),
+  DESC("desc");
+
+  private final String condition;
+
+  /**
+   * 입력 값을 Enum Type으로 변경(기본값은 DESC - 최신순)
+   *
+   * @param value 타입 이름
+   * @return 값에 해당하는 SortType Enum
+   */
+  public static SortType getSortType(String value) {
+
+    if (value == null || value.isEmpty()) {
+      return SortType.DESC;
+    }
+
+    return Arrays.stream(SortType.values()).
+        filter(x -> x.getCondition().equals(value)).findAny()
+        .orElseThrow(() -> new ConditionException(ConditionError.INVALID_SORT_TYPE));
+  }
+}
+
