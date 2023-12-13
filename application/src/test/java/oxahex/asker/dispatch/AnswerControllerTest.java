470aaa4fa81cf5424ccaa50276d910ec5538956a
oxahex <oxahex@gmail.com>
2023-12-13 22:17:56 +0900

    test: 답변 조회 기능 테스트 코드 수정

--- application/src/test/java/oxahex/asker/dispatch/AnswerControllerTest.java
+++ application/src/test/java/oxahex/asker/dispatch/AnswerControllerTest.java
@@ -1,11 +1,15 @@
 package oxahex.asker.dispatch;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,11 +27,12 @@ import org.springframework.transaction.annotation.Transactional;
 import oxahex.asker.dispatch.dto.AnswerDto;
 import oxahex.asker.dispatch.dto.AnswerDto.AnswerInfoDto;
 import oxahex.asker.dispatch.dto.AnswerDto.AnswerReqDto;
+import oxahex.asker.dispatch.dto.AnswerDto.PostedAnswersDto;
 import oxahex.asker.domain.answer.Answer;
 import oxahex.asker.domain.ask.Ask;
 import oxahex.asker.domain.ask.AskType;
+import oxahex.asker.domain.condition.SortType;
 import oxahex.asker.domain.dispatch.Dispatch;
-import oxahex.asker.domain.dispatch.DispatchRepository;
 import oxahex.asker.domain.user.User;
 import oxahex.asker.domain.user.UserRepository;
 import oxahex.asker.mock.MockUser;
@@ -46,11 +51,11 @@ class AnswerControllerTest extends MockUser {
   @Autowired
   private UserRepository userRepository;
 
-  @Autowired
-  private DispatchRepository dispatchRepository;
-
   @MockBean
   private DispatchService dispatchService;
+
+  @MockBean
+  private AnswerService answerService;
 
   private User askUser;
   private User answerUser;
@@ -122,5 +127,69 @@ class AnswerControllerTest extends MockUser {
 
     // then
     resultActions.andExpect(status().isUnauthorized());
+  }
+
+  @Test
+  @DisplayName("답변 내역 조회 API 접근 성공 - 비로그인 유저의 경우 답변 내역 조회 API에 접근할 수 있다.")
+  public void getUserAnswers_success_without_login() throws Exception {
+
+    // given: 작성한 답변 없음
+    given(answerService.getPostedAnswers(anyLong(), any(SortType.class)))
+        .willReturn(new PostedAnswersDto());
+
+    // when
+    ResultActions resultActions = mockMvc.perform(get("/api/answers")
+        .param("userId", answerUser.getId().toString())
+        .param("sort", SortType.DESC.getCondition())
+        .contentType(MediaType.APPLICATION_JSON));
+
+    // then
+    resultActions.andExpect(status().isOk());
+  }
+
+  @Test
+  @WithUserDetails(value = "answerer@gmail.com", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+  @DisplayName("답변 내역 조회 API 접근 성공 - 로그인 유저의 경우 답변 내역 조회 API에 접근할 수 있다.")
+  public void getUserAnswers_success_with_login_user() throws Exception {
+
+    // given: 작성한 답변 없음
+    given(answerService.getPostedAnswers(anyLong(), any(SortType.class)))
+        .willReturn(new PostedAnswersDto());
+
+    // when
+    ResultActions resultActions = mockMvc.perform(get("/api/answers")
+        .param("userId", answerUser.getId().toString())
+        .param("sort", SortType.DESC.getCondition())
+        .contentType(MediaType.APPLICATION_JSON));
+
+    // then
+    resultActions.andExpect(status().isOk());
+  }
+
+  @Test
+  @DisplayName("답변 내역 조회 시 작성한 답변이 없는 경우 Empty List를 반환한다.")
+  public void getUserAnswers_success_if_null() throws Exception {
+
+    // given: 받은 질문 없음
+    PostedAnswersDto postedAnswersDto =
+        AnswerDto.fromEntityToPostedAnsweredDto(answerUser, new ArrayList<Answer>());
+
+    // given: 작성한 답변 없음
+    given(answerService.getPostedAnswers(anyLong(), any(SortType.class)))
+        .willReturn(postedAnswersDto);
+
+    // when
+    ResultActions resultActions = mockMvc.perform(get("/api/answers")
+        .param("userId", answerUser.getId().toString())
+        .param("sort", SortType.DESC.getCondition())
+        .contentType(MediaType.APPLICATION_JSON));
+
+    // then
+    resultActions.andExpect(status().isOk());
+    resultActions.andExpect(jsonPath("$.data.answerUser.id").value(answerUser.getId()));
+    resultActions.andExpect(jsonPath("$.data.answerUser.name").value(answerUser.getName()));
+    // 질문 목록은 Empty List
+    resultActions.andExpect(jsonPath("$.data.answers").isArray());
+    resultActions.andExpect(jsonPath("$.data.answers").isEmpty());
   }
 }
